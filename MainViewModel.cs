c32974a5e0148db387c1d8f44aa3e4f5e3b53941
Daniel Epaminondas Bonete <daniel.epaminondas@gmail.com>
2025-02-08 00:24:52 -0400

    Primeiro commit

---
+++ MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Windows.Media;
+
+namespace GameClient
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Color _Color01;
+
+        public Color Color01
+        {
+            get => _Color01;
+            set
+            {
+                if (_Color01 != value)
+                {
+                    _Color01 = value;
+                    OnPropertyChanged("Color01");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using GameClient;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace GameClient
+
+{
+    public partial class MainWindow : Window
+    {
+
+        // Reinicia o vídeo automaticamente sem pausas
+        private void VideoPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            VideoPlayer.Position = TimeSpan.Zero;
+            VideoPlayer.Play();
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ResizeImage();
+            DataContext = new MainViewModel();
+
+        }
+
+
+        // Método que pode ser chamado para redimensionar a imagem em tempo de execução
+        private void ResizeImage()
+        {
+            myImage.Width = 300;  // Novo valor para a largura
+            myImage.Height = 200; // Novo valor para a altura 
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Aqui você pode definir o comportamento de saída, como fechar a janela
+            Application.Current.Shutdown();
+        }
+
+        // No arquivo MainWindow.xaml.cs
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove(); // Permite arrastar a janela
+            }
+        }
+    
+
+    }
+}
